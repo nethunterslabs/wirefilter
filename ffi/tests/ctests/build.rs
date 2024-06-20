d8d2e9912d0f5a731d5c3300854a9e5b4140c04e
veeshi <31014797+veeshi@users.noreply.github.com>
2024-06-20 02:42:36 +0100

    remove unusued ffi and wirefilter-parser crates

--- ffi/tests/ctests/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    cc::Build::new()
-        .include("../../include")
-        .file("src/tests.c")
-        .warnings(true)
-        .extra_warnings(true)
-        .warnings_into_errors(true)
-        .compile("wirefilter_ffi_ctests");
-}
